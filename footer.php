5e5552079377c86df6e162667e17c6e3373b42e7
Mohammed Basith <mhdbazi@gmail.com>
2017-03-04 17:33:27 +0530

    Restructed

---
+++ footer.php
@@ -0,0 +1,19 @@
+  <footer>
+          
+          <div class=" colGreenbg footer-copyright">
+            <div class="container">
+            <br>
+                <a class="grey-text text-lighten-4 center-align" href="#!"><p>© 2014 Copyright Text</p></a>
+            <br>
+            </div>
+          </div>
+        </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,54 @@
+
+
+
+
+<main>
+           <center>
+     
+      <div class="section"></div>
+
+      <h5 class="colGreen jobportalHeading">JOB PORTAL</h5>
+      <div class="section"></div>
+
+      <div class="container">
+        <div class="z-depth-1 white  row" style="display: inline-block; padding: 32px 48px 0px 48px; border-bottom: 3px solid #00D494;border-top: 3px solid #00D494;">
+
+          <form class="col s12" method="post">
+            <div class='row'>
+              <div class='col s12'>
+                  <h5 class="colGreen loginHead">Login</h5>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+                <input class='validate' type='email' name='email' id='email' />
+                <label for='email'>Enter your email</label>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+                <input class='validate' type='password' name='password' id='password' />
+                <label for='password'>Enter your password</label>
+              </div>
+              <label style='float: right;'>
+								<a class='colGreen' href='#!'><b>Forgot Password?</b></a>
+							</label>
+            </div>
+
+            <br />
+            <center>
+              <div class='row'>
+                <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect colGreenbg'>Login</button>
+              </div>
+            </center>
+            <a class='colGreen' href="#!">Create account</a>
+              <br>
+          </form>
+        </div>
+      </div>
+      
+    </center>
+
+    </main>
