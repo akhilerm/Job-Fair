281af28974608d960d9221e6f6df0590f8206cc1
Mohammed Basith <mhdbazi@gmail.com>
2017-03-05 19:48:37 +0530

    alignment messages

--- dashboard_view.php
+++ dashboard_view.php
@@ -3,9 +3,6 @@ include("header.php");
 ?>
 
 <main>
-  <button class="logout btn-flat waves-effect waves-light" style="border:1px solid #00d494;color:#00d494" type="submit" name="action">Logout
-                
-</button>    
  <div class="container-fluid">
 
   <div class="row" style="margin-top:5px;">
@@ -20,38 +17,41 @@ include("header.php");
     <div id="test1" class="col s12">
         
          <div class="row">
-        <div class="col s12 l12">
-          <div class="card">
-            
-            <div class="card-content">
-               <div class="row">
-                   <h5 class="center-align colGreen" style="text-weight:bold;">General Notifications</h5>
-                   <hr class="style1">       
-                </div>
+        <!-- Starts here -->
+                  <div class="col s12 l12">
+              <div class="card">
                 
-                <div class="row colGreen" style="font-weight:bold;">
-                   <ul style="padding-left:24px;">
-                        <li>
-                               <h6>orem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elit</h6><br>
-                        </li>
-                      
-                       <li>
-                                <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elit.</h6><br>
-                        </li>
-                      
-                       <li>
-                                <h6>Aliquam tincidunt mauris eu risus.</h6> <br>
-                        </li>
-                       
-                    </ul>        
+                <div class="card-content">
+                   <div class="row">
+                       <h5 class="center-align colGreen" style="text-weight:bold;">General Notifications</h5>
+                       <hr class="style1">       
+                    </div>
+                    
+                    <div class="row colGreen" style="font-weight:bold;">
+                       <ul style="padding-left:24px;">
+                            <li>
+                                   <h6>orem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elit</h6><br>
+                            </li>
+                          
+                           <li>
+                                    <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elitLorem ipsum dolor sit amet, consectetuer adipiscing elit.</h6><br>
+                            </li>
+                          
+                           <li>
+                                    <h6>Aliquam tincidunt mauris eu risus.</h6> <br>
+                            </li>
+                           
+                        </ul>        
+                    </div>
+                    
+                   
                 </div>
-                
-               
+
+              </div>
             </div>
 
-          </div>
-        </div>
-        <div class="col s12 l3">
+      
+    <div class="col s12 l3">
           <div class="card">
             <div class="card-image">
               <img src="images/atm.png" style="border-bottom:1px solid #00d494">
@@ -59,40 +59,317 @@ include("header.php");
             </div>
             <div class="card-content">
                <div class="row">
-                   <h5 class="center-align colGreen">Atletico</h5>
+                   <h5 class="center-align colGreen">ALLIANZ CORNHILL INFORMATION SERVICES PVT</h5>
                    <hr class="style1">
-                   <div class="col s6 l6">
+                   <div class="col s12 l12">
                         <h6 class="colGreen">CTC</h6>
                    </div>
                    
-                   <div class="col s6 l6">
-                        <h6>3LPA</h6>
+                   <div class="col s12 l12">
+                        <h6>2.5-3.5LPA</h6>
                    </div>         
                 </div>
                 
                 <div class="row">
-                   <div class="col s6 l6">
+                   <div class="col s12 l12">
                         <h6 class="colGreen">Who can apply</h6>
                    </div>
                    
-                   <div class="col s6 l6">
-                        <h6>B-tech,B-com</h6>
+                   <div class="col s12 l12">
+                        <h6>All graduates and non graduates(Plus two/VHSE/Diploma)</h6>
                    </div>         
                 </div>
                 
                 <div class="row">
-                   <div class="col s6 l6">
+                   <div class="col s12 l12">
                         <h6 class="colGreen">Eligibility</h6>
                    </div>
                    
-                   <div class="col s6 l6">
-                        <h6>60% mark or 6% gpa</h6>
+                   <div class="col s12 l12">
+                        <h6>Fresher's or Experienced with Good Command over English</h6>
                    </div>         
                 </div>
             </div>
 
           </div>
         </div>
+
+             
+             
+     <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">SRISHTI INNOVATIVE</h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">CTC</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>To be decided</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>B.tech:Computer scince ,Electronics and communication and Electrical and electronics enginnering<br>Bsc/Msc:Computer scince <br>MCA/BCA</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>above 65 percentage or 6.5</h6>
+                       
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div>            
+             
+             
+             
+             
+             
+    <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">PIVOT <br/> SYSTEMS</h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Salary</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>24000/month</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>B.tech : Computer sceince <br>Bsc/Msc :computer scince<br>BCA and MCA</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>70 percentage or above 7.0 cgpa</h6>
+                       <br>
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div>
+ <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">TOONZ ANIMATIONS INDIA PVT Ltd</h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Salary</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>10000 -20000 per month</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>All graduates </h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>above60 percentage or 6.0 cgpa</h6>
+                       <br>
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div>
+             
+        </div>
+        <div class="row">
+<div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">FAZZA Information Technology</h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">CTC</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>To be decided later</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>For B.tech/M.tech: computer scince ,electronics and communication - Android Developer(2)
+Php developers,Asp.Net Developers,Web designers<br>MCA/BCA -Asp.Net Developers <br>Bsc/Msc/Bcom :Jr. Admin Executive
+Sales Excutives
+
+ </h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>Candidates with backlogs can apply</h6>
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div>
+            
+
+ <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">PERFOMATIX SOLUTIONS PVT LTD</h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">CTC</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>To be decided later</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>B-tech: Computer science,Electronics and Communication<br>M-tech:Computer Science Engineering ,Communication Systems<br> Bsc/Msc : Computer science<br>BCA and MCA </h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>65 percentage or 6.5 And Above </h6>
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div>   
+
+            
+
+ <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/atm.png" style="border-bottom:1px solid #00d494">
+            
+            </div>
+            <div class="card-content">
+               <div class="row">
+                   <h5 class="center-align colGreen">Cognub Decision Solutions Pvt. Ltd. </h5>
+                   <hr class="style1">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">CTC</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>To be decided </h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Who can apply</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>B.tech:Computer scince <br>MCA/BCA</h6>
+                   </div>         
+                </div>
+                
+                <div class="row">
+                   <div class="col s12 l12">
+                        <h6 class="colGreen">Eligibility</h6>
+                   </div>
+                   
+                   <div class="col s12 l12">
+                        <h6>Above 70 or 7.0 cgpa</h6>
+                   </div>         
+                </div>
+            </div>
+
+          </div>
+        </div> 
+                          
+<!-- Ends here -->
       </div>
       
       
@@ -135,21 +412,12 @@ include("header.php");
           <tr>
             <td>Alan</td>
             <td>Jellybean</td>
-            <td> 
-              <button class="btn-flat waves-effect waves-light" style="border:1px solid #00d494;color:#00d494" type="submit" name="action">Apply
-                <i class="material-icons right">send</i>
-              </button> 
-            </td>
           
           </tr>
           <tr>
             <td>Jonathan</td>
             <td>Lollipop</td>
-            <td> 
-              <button class="btn-flat waves-effect waves-light" style="border:1px solid #00d494;color:#00d494" type="submit" name="action">Apply
-                <i class="material-icons right">send</i>
-              </button> 
-            </td>
+          
           </tr>
         </tbody>
       </table>
@@ -163,39 +431,11 @@ include("header.php");
       
       
       <div class="row">
-      <div class="profileCard" style="margin:0 auto;">
+      <div class="" style="margin:0 auto; width:80%;">
         <div class="card-panel">
-            
-        <table class=" striped centered">
-        <thead>
-          <tr >
-              <h6 class="colGreen" style="text-align:center;font-weight:bold"> Personal Profile</h6>
-             
-              
-          </tr>
-        </thead>
-
-        <tbody class="colGreen">
-          <tr >
-            <td>Alvin</td>
-            <td>Eclair</td>
-            
-            
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-         
-          
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            
-          </tr>
-        </tbody>
-      </table>
-      
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
         </div>
       </div>
     </div>
