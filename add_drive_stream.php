0a4563dafa256a3b4ad75616f7d9bc7d93d8628a
Akhil Mohan <akhilerm@gmail.com>
2017-03-06 18:14:50 +0530

    query updatE

--- add_drive_stream.php
+++ add_drive_stream.php
@@ -2,8 +2,7 @@
 	require("db_connect.php");
 	require("admin_query.php");
 	$company_id = $_POST['company3'];
-	$course_id = $_POST['course3'];
 	$stream_id = $_POST['stream3'];
-	echo add_drive_stream($company_id,$course_id,$stream_id,$con);
+	add_drive_stream($company_id,$stream_id,$con);
 	header("location:admin_panel.php");
 ?>
