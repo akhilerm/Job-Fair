0b1afd3c42379feaa5b0fb33ec4716a1e8cdc8cc
Akhil Mohan <akhilerm@gmail.com>
2017-03-06 16:56:53 +0530

    stream_list updated

--- stream_list.php
+++ stream_list.php
@@ -1,22 +1,22 @@
-<?php 
-  session_start();
-  require_once("db_connect.php");
-  require_once("query.php");
-
-  $course_id=$_POST['course_id'];
-  //if($_SESSION['FLAG_STREAM']==1)
-  //{    
-    // $_SESSION['FLAG_STREAM']==0;
-     $result=fetch_stream($course_id,$con);
-     //if($result!=-1)
-        while($row=$result->fetch_assoc()){
-          $temp1=$row['id'];
-          $temp2=$row['stream_name'];
-          ?>
-          <option value="<?php echo $temp1; ?>"><?php echo $temp2; ?></option>
-          <?php
-        //}
-  //}
-  //else
-    //header("location:index.php");
+<?php
+  require("db_connect.php");
+  $course_id = $_POST['crID'];
+  $query = "SELECT * from stream where course_id = $course_id";
+  $result = $con->query($query);
+  if($result){
+    while ($row=$result->fetch_assoc()) 
+    {
+      $temp1=$row['id'];
+      $temp2=$row['stream_name'];
+      ?>
+      <option value ="<?php echo $temp1; ?>"><?php echo $temp2; ?></option>"
+      <?php
+    }
+  }
+  else
+  {
+    ?>
+    <option value = '0'>Not Available</option>
+    <?php
+  }
 ?>
