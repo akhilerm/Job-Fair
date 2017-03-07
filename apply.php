f50810693b52ac27db5af15a8d59b47d70871847
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-07 15:01:10 +0530

    apply.php updated removed a bug

--- apply.php
+++ apply.php
@@ -7,52 +7,51 @@
 	    if($_SESSION['LoggedINUser']==1)
 	    {
 	    	$user_id=$_SESSION['USER_ID'];
-	    	$query1="select * from applied where student_id=".$user_id;
-	    	$result1=$con->query($query1);
-	    	if($result1)
-	    	{
-	    		if($result1->num_rows>0)
-	    		{
-			    	$company=$_POST['C_ID'];
-			    	$query="select * from applied where student_id=".$user_id." and company_id=".$company;
-			    	$result=$con->query($query) ;				
-					if($result)
-					{
-						if($result->num_rows>0)
-						{
-							echo "You Have Already Registered.";
-						}
-						else
-						{
-							$query= "insert into applied (student_id, company_id) values (".$user_id.",".$company.")";
+	    	$company=$_POST['C_ID'];
+	    	$query="select * from applied where student_id=".$user_id." and company_id=".$company;
+	    	$result=$con->query($query) ;				
+			if($result)
+			{
+				if($result->num_rows>0)
+				{
+					echo "You Have Already Registered.";
+				}
+				else
+				{
+					$query1="select count(*) as count from applied where student_id=".$user_id;
+			    	$result1=$con->query($query1);
+			    	if($result1)
+			    	{
+			    		$row1=$result1->fetch_assoc();
+			    		if($row1['count']<4)
+			    		{
+					    	$query= "insert into applied (student_id, company_id) values (".$user_id.",".$company.")";
 				    		$result=$con->query($query);
 							if($result)
 							{
-									echo "Successfully Applied" ;	
+								echo "Successfully Applied" ;	
 							}
 							else
 							{
 								echo "Something Went Wrong.Please Try Again.";	
 							}
 						}
+						else
+						{
+							
+							echo "Maximum of 4 companies applied.";
+							
+						}
 					}
 					else
 					{
-						?>
-							You Have Already Registered.
-						<?php
+						header("location:user_account.php");	
 					}
-				}
-				else
-				{
-					?>
-						Maximum of 4 companies applied.
-					<?php
 				}
 			}
 			else
 			{
-				header("location:user_account.php");	
+				echo "Something Went Wrong.Please Try Again.";
 			}
 		}
 		else
