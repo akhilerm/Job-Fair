88d591fc2eba652205f864b881f9715fdc3637f5
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-11 00:46:41 +0530

    testing

--- update_pay.php
+++ update_pay.php
@@ -10,22 +10,23 @@
 			{
 				$update_id=cleanup($_POST['update_id'],$con);
 				$query = "UPDATE user set trans_id=".$update_id." where id=".$_SESSION['USER_ID'];
-				$result=$con->query($query);
-				header("location:user_account.php");	
+				echo $query;
+				//$result=$con->query($query);
+				//header("location:user_account.php");	
 			}
 			else
 			{
-				header("location:user_account.php");
+				//header("location:user_account.php");
 			}
 		}
 		else
 		{
-			header("location:user_account.php");
+			//header("location:user_account.php");
 		}
 	}
 	else
 	{
-		header("location:user_account.php");
+		//header("location:user_account.php");
 	}
 
 ?>
